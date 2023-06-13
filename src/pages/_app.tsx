174480f5815dd773fa423ebff6bbed1fd6fdf651
Wellinton Alexandre <wellinton1067@gmail.com>
2023-06-12 21:28:43 -0300

    added container direct in styled component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,13 +14,13 @@ export default function App({ Component, pageProps }: AppProps) {
   if (router.pathname === '/_error') return <Component {...pageProps} />
 
   return (
-    <DarkModeProvider>
-      <>
-        <GlobalStyle font={font} />
+    <>
+      <GlobalStyle font={font} />
+      <DarkModeProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </>
-    </DarkModeProvider>
+      </DarkModeProvider>
+    </>
   )
 }
