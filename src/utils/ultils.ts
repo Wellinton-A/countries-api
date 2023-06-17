0514717085297a7e1c4a375d6f6fc11c86059dc8
Wellinton Alexandre <wellinton1067@gmail.com>
2023-06-16 23:17:27 -0300

    removed local data

--- src/utils/ultils.ts
+++ src/utils/ultils.ts
@@ -1,15 +1,3 @@
-// import path from 'path'
-// import fs from 'fs/promises'
-// import process from 'process'
-
-// export const getLocalData = async (): Promise<CountryData[]> => {
-//   const filePath = path.join(process.cwd(), 'data', 'data.json')
-//   const jsonData = await fs.readFile(filePath)
-//   const jsonString: string = jsonData.toString('utf8')
-//   const dataJson: CountryData[] = JSON.parse(jsonString)
-//   return dataJson
-// }
-
 export const fethcAllData = async () => {
   const response = await fetch('https://restcountries.com/v3.1/all')
   if (!response.ok) throw new Error('Unable to fetch data')
